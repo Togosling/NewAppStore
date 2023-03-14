237b75aee109109cbbd49e64512bfd3bce7b01df
Togosling <114942562+Togosling@users.noreply.github.com>
2023-03-14 20:29:37 +0800

    version1

---
+++ NewAppStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewAppStore
+//
+//  Created by Тагай Абдылдаев on 14/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
