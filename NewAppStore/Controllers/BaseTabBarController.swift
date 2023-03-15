50e52b76e4485e50bdb673df09419c26aca26919
Togosling <114942562+Togosling@users.noreply.github.com>
2023-03-15 11:29:26 +0800

    Day 2

---
+++ NewAppStore/Controllers/BaseTabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  BaseTabBarController.swift
+//  NewAppStore
+//
+//  Created by Тагай Абдылдаев on 14/3/23.
+//
+
+import UIKit
+
+class BaseTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        viewControllers = [createNavController(viewController: UIViewController(), title: "Today", imageName: "today_icon"),
+                           createNavController(viewController: UIViewController(), title: "Apps", imageName: "apps"),
+                           createNavController(viewController: SearchController(), title: "Search", imageName: "search")]
+    }
+    
+    fileprivate func createNavController(viewController: UIViewController, title: String, imageName: String) -> UIViewController {
+        viewController.view.backgroundColor = .white
+        viewController.navigationItem.title = title
+        let navController = UINavigationController(rootViewController: viewController)
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = UIImage(named: imageName)
+        navController.navigationBar.prefersLargeTitles = true
+        return navController
+    }
+    
+}
---
+++ NewAppStore/Controllers/SearchController.swift
@@ -0,0 +1,24 @@
+//
+//  SearchController.swift
+//  NewAppStore
+//
+//  Created by Тагай Абдылдаев on 15/3/23.
+//
+
+import UIKit
+
+class SearchController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    init() {
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
