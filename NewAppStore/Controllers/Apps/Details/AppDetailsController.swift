635c4665c4a3cce66ee7db9ac640af53ea8c3fe5
Togosling <114942562+Togosling@users.noreply.github.com>
2023-03-24 12:50:16 +0800

    DetailsCell

--- NewAppStore/Controllers/Apps/Details/AppDetailsController.swift
+++ NewAppStore/Controllers/Apps/Details/AppDetailsController.swift
@@ -13,7 +13,6 @@ class AppDetailsController: UICollectionViewController, UICollectionViewDelegate
         didSet {
             let urlString = "https://itunes.apple.com/lookup?id=\(appId ?? "")"
             Service.shared.fetchGenericJsonData(urlString: urlString) { (result: SearchResult) in
-//                print(result.results.first?.formattedPrice)
             }
         }
     }
