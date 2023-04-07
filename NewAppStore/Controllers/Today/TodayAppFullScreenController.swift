5b3219ea036b9c5e61e34b3151811b3484a6daa9
Togosling <114942562+Togosling@users.noreply.github.com>
2023-04-07 10:41:36 +0800

    Blur effect

--- NewAppStore/Controllers/Today/TodayAppFullScreenController.swift
+++ NewAppStore/Controllers/Today/TodayAppFullScreenController.swift
@@ -7,27 +7,110 @@
 
 import UIKit
 
-class TodayAppFullScreenController: UITableViewController {
+class TodayAppFullScreenController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var todayItem: TodayItem?
+    
+    let tableView = UITableView(frame: .zero, style: .plain)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.addSubview(tableView)
+        tableView.fillSuperview()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         tableView.tableFooterView = UIView()
         tableView.separatorStyle = .none
-        tableView.isUserInteractionEnabled = false
+        tableView.allowsSelection = false
+        
+        setupFloatingView()
+    }
+    
+    let floatingView = UIView()
+    
+    fileprivate func setupFloatingView() {
+        floatingView.clipsToBounds = true
+        floatingView.layer.cornerRadius = 16
+        view.addSubview(floatingView)
+        floatingView.anchor(top: nil, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor, padding: .init(top: 0, left: 16, bottom: -90, right: 16), size: .init(width: 0, height: 90))
+        
+        let blurEffect = UIVisualEffectView(effect: UIBlurEffect(style: .regular))
+        floatingView.addSubview(blurEffect)
+        blurEffect.fillSuperview()
+        
+        let imageView: UIImageView = {
+            let iv = UIImageView()
+            iv.image = todayItem?.image
+            iv.widthAnchor.constraint(equalToConstant: 64).isActive = true
+            iv.heightAnchor.constraint(equalToConstant: 64).isActive = true
+            iv.layer.cornerRadius = 12
+            iv.clipsToBounds = true
+            return iv
+        }()
+        
+        let nameLabel: UILabel = {
+            let label = UILabel()
+            label.text = todayItem?.category
+            label.font = .boldSystemFont(ofSize: 18)
+            return label
+        }()
+        
+        let descriptionLabel: UILabel = {
+            let label = UILabel()
+            label.text = todayItem?.title
+            label.font = .systemFont(ofSize: 13)
+            return label
+        }()
+        
+        let getButton: UIButton = {
+            let button = UIButton(type: .system)
+            button.setTitle("GET", for: .normal)
+            button.titleLabel?.font = .boldSystemFont(ofSize: 14)
+            button.backgroundColor = UIColor(white: 0.95, alpha: 1)
+            button.setTitleColor(.systemBlue, for: .normal)
+            button.layer.cornerRadius = 14
+            button.widthAnchor.constraint(equalToConstant: 80).isActive = true
+            button.heightAnchor.constraint(equalToConstant: 32).isActive = true
+            return button
+        }()
+        
+        let labelStackView = UIStackView(arrangedSubviews: [nameLabel,descriptionLabel])
+        labelStackView.axis = .vertical
+        
+        let stackView = UIStackView(arrangedSubviews: [imageView,labelStackView, getButton])
+        stackView.spacing = 12
+        stackView.alignment = .center
+        
+        floatingView.addSubview(stackView)
+        stackView.fillSuperview()
         
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollView.contentOffset.y > 100 {
+            UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: .curveEaseOut) {
+                self.floatingView.transform = .init(translationX: 0, y: -116)
+            }
+        } else {
+            UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: .curveEaseOut) {
+                self.floatingView.transform = .identity
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 2
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.row == 0 {
-           let headerCell = AppFullScreenHeaderCell()
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.item == 0{
+            let headerCell = AppFullScreenHeaderCell()
             headerCell.todayCell.todayItem = todayItem
+            headerCell.todayCell.layer.cornerRadius = 0
+            headerCell.todayCell.backgroundView?.layer.cornerRadius = 0
             headerCell.clipsToBounds = true
             return headerCell
         }
@@ -35,13 +118,11 @@ class TodayAppFullScreenController: UITableViewController {
         return cell
     }
     
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         if indexPath.row == 0 {
             return 500
         }
-        return super.tableView(tableView, heightForRowAt: indexPath)
+        return UITableView.automaticDimension
     }
-    
-    
     
 }
